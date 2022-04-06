b4e2a8bb518fb60192149807cef500baaf7947dc
Duncan Awerbuck <duk444@gmail.com>
2022-04-06 08:52:51 +0100

    solution to exercise 4: "tic tac toe"

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,8 +4,7 @@
 import * as React from 'react';
 
 function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null);
+  const [squares, setSquares] = React.useState(Array(9).fill(null));
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
@@ -14,28 +13,23 @@ function Board() {
   // 💰 I've written the calculations for you! So you can use my utilities
   // below to create these variables
 
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
   function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
+    // bail if game already over
+    const {gameStatus, nextValue} = getStatus();
+    if (!gameStatus.startsWith('Next player:')) return;
+
+    // bail if square already taken
+    if (squares[square] !== null) return;
+
+    const updatedSquares = [...squares];
+
+    updatedSquares[square] = nextValue;
+    setSquares(updatedSquares);
   }
 
   function restart() {
     // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
+    setSquares(Array(9).fill(null));
   }
 
   function renderSquare(i) {
@@ -46,10 +40,16 @@ function Board() {
     );
   }
 
+  function getStatus() {
+    const nextValue = calculateNextValue(squares);
+    const winner = calculateWinner(squares);
+    const gameStatus = calculateStatus(winner, squares, nextValue);
+    return {gameStatus, nextValue};
+  }
+
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
+      <div className="status">{getStatus().gameStatus}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
